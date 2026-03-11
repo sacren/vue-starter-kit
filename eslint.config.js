e4e8f06f71324951713221b38dff710674d23687
Joe Tannenbaum <joe.tannenbaum@laravel.com>
2026-03-11 10:41:51 -0400

    ESLint/formatting updates (#321)
    Co-authored-by: WendellAdriel <11641518+WendellAdriel@users.noreply.github.com>
    Co-authored-by: joetannenbaum <2702148+joetannenbaum@users.noreply.github.com>

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,25 @@
+import stylistic from '@stylistic/eslint-plugin';
 import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript';
 import prettier from 'eslint-config-prettier/flat';
 import importPlugin from 'eslint-plugin-import';
 import vue from 'eslint-plugin-vue';
+
+const controlStatements = [
+    'if',
+    'return',
+    'for',
+    'while',
+    'do',
+    'switch',
+    'try',
+    'throw',
+];
+const paddingAroundControl = [
+    ...controlStatements.flatMap((stmt) => [
+        { blankLine: 'always', prev: '*', next: stmt },
+        { blankLine: 'always', prev: stmt, next: '*' },
+    ]),
+];
 
 export default defineConfigWithVueTs(
     vue.configs['flat/essential'],
@@ -46,6 +64,17 @@ export default defineConfigWithVueTs(
         },
     },
     {
+        plugins: {
+            '@stylistic': stylistic,
+        },
+        rules: {
+            '@stylistic/padding-line-between-statements': [
+                'error',
+                ...paddingAroundControl,
+            ],
+        },
+    },
+    {
         ignores: [
             'vendor',
             'node_modules',
@@ -56,7 +85,17 @@ export default defineConfigWithVueTs(
             'resources/js/actions/**',
             'resources/js/components/ui/*',
             'resources/js/routes/**',
+            'resources/js/wayfinder/**',
         ],
     },
     prettier, // Turn off all rules that might conflict with Prettier
+    {
+        plugins: {
+            '@stylistic': stylistic,
+        },
+        rules: {
+            curly: ['error', 'all'],
+            '@stylistic/brace-style': ['error', '1tbs', { allowSingleLine: false }],
+        },
+    },
 );
